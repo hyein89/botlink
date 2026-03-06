0cf8bff381043215198bf05a8e53d3931f8f5c7e
hyein89 <bihontai@gmail.com>
2026-03-06 12:15:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 export const appConfig = {
   DOMAIN: 'copbhghuytr.eu.org',
-  OFFER1: 'https://google.com' // Ganti target Offer 1 lo di sini
+  OFFER1: 'https://shorturl.at/6cNjC' 
 };
