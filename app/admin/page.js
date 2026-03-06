83e80438770594ce383689fc2e7ebc7576d06aa0
hyein89 <bihontai@gmail.com>
2026-03-06 11:38:37 +0700

    Update page.js

--- app/admin/page.js
+++ app/admin/page.js
@@ -111,7 +111,7 @@ export default function AdminPage() {
           
           <div className="row" style={{ marginBottom: '20px' }}>
             <div className="col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2" style={{ display: 'flex', justifyContent: 'flex-end' }}>
-              // ... scroll ke bawah ke bagian tombol "List Link" ...
+              
 <Link href="/list" className="btn btn-default" style={{ borderRadius: '8px', fontWeight: '500', display: 'flex', alignItems: 'center', gap: '5px', border: '1px solid #ddd', color: '#555', textDecoration: 'none' }}>
    <span className="material-icons notranslate" translate="no" style={{ fontSize: '18px' }}>format_list_bulleted</span>
    List Link
