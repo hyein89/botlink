21829750ea1ff171effa84ad8a8988ca66d0a701
hyein89 <bihontai@gmail.com>
2026-03-07 20:29:39 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js
 export const appConfig = {
   DOMAIN: 'copbhghuytr.eu.org',
-  OFFER1: 'https://shorturl.at/6cNjC' 
+  OFFER1: 'https://abovemyselfsadness.com/vje7pf3gi?key=07c6b0205758fe4645594fb4ab8ea5ee' 
 };
